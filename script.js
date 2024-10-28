ab38685ccb2a163a20dacb8b1f6c06f46d5cab87
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-10-28 13:55:57 +0700

    Complete gui version of the game`

--- script.js
+++ script.js
@@ -1,23 +1,131 @@
-function createPlayer(name, signatureMove) {
-    const getPlayerMove = function(move = "") {
-        const arrIndex = move.split(',');
-        return arrIndex;
-    };
-    return { name, signatureMove, getPlayerMove };
-}
-
 const game = (function() {
     const arrBoard = [
         ['', '', ''],
         ['', '', ''],
         ['', '', '']
     ];
-    const player1 = createPlayer(prompt("What is player 1 name?"), "O");
-    const player2 = createPlayer(prompt("What is player 2 name?"), "X");
+    let player1;
+    let player2;
+    let winner;
 
-    const setMove = function(rowIndex, columnIndex, signatureMove) {
-        arrBoard[rowIndex][columnIndex] = signatureMove;
-    };
+    function createPlayer(name, signatureMove) {
+        const playerDoMove = function(rowIndex, columnIndex, signatureMove) {
+            arrBoard[rowIndex][columnIndex] = signatureMove;
+        };
+
+        return { name, signatureMove, playerDoMove };
+    }
+
+    const domBuilder = function() {
+        const openModal = document.querySelector(".show-modal");
+        const closeModal = document.querySelector("dialog form button");
+        const modal = document.querySelector("dialog");
+        const form = document.querySelector("dialog form");
+        const boxes = document.querySelectorAll(".boxes");
+        const infoText = document.querySelector(".info-text");
+
+        function resetGame() {
+            winner = undefined;
+            player1 = undefined;
+            player2 = undefined;
+            for (let row = 0; row < arrBoard.length; row++) {
+                for (let col = 0; col < arrBoard[row].length; col++) {
+                    arrBoard[row][col] = '';
+                }
+            }
+
+            removeListeners();
+            openModal.textContent = "Play again?";
+            openModal.style.display = "block";
+        }
+
+        function removeListeners() {
+            boxes.forEach(element => element.removeEventListener('click', element.fn, false));
+        }
+
+        const applyListeners = function() {
+            openModal.addEventListener("click", () => {
+                modal.showModal();
+            });
+            closeModal.addEventListener("click", () => {
+                modal.close();
+            });
+            form.addEventListener("submit", () => {
+                let turn = 0;
+                boxes.forEach(element => element.textContent = '');
+                player1 = createPlayer(document.getElementById("playerOneName").value, "X");
+                player2 = createPlayer(document.getElementById("playerTwoName").value, "O");
+                openModal.style.display = "none";
+                document.querySelector(".tic-tac-toe").style.display = "grid";
+                infoText.textContent = `${player1.name}'s turn, your signature move is '${player1.signatureMove}'`;
+
+                boxes.forEach(element => element.addEventListener("click", element.fn = function fn() {
+                    if (turn === 0) {
+                        player1.playerDoMove(
+                            element.getAttribute("data-row-index"),
+                            element.getAttribute("data-col-index"),
+                            player1.signatureMove
+                        );
+                        console.table(arrBoard);
+                        element.textContent = player1.signatureMove;
+                        winner = checkBoard();
+                        console.log(player1.signatureMove);
+                        console.log(player2.signatureMove);
+                        console.log(winner);
+                        if (winner === player1.signatureMove) {
+                            infoText.textContent = `${player1.name} WINS!`;
+                            resetGame();
+                             turn = 0;
+                        } else if (winner === player2.signatureMove) {
+                            infoText.textContent = `${player2.name} WINS!`;
+                            resetGame();
+                             turn = 0;
+                        } else if (winner === 'T') {
+                            infoText.textContent = "It's a TIE!";
+                            resetGame();
+                             turn = 0;
+                        } else {
+                            infoText.textContent = `${player2.name}'s turn, your signature move is '${player2.signatureMove}'`;
+                        }
+                        element.removeEventListener('click', element.fn, false);
+                        turn++;
+                    } else {
+                        player2.playerDoMove(
+                            element.getAttribute("data-row-index"),
+                            element.getAttribute("data-col-index"),
+                            player2.signatureMove
+                        );
+                        console.table(arrBoard);
+                        element.textContent = player2.signatureMove;
+                        winner = checkBoard();
+                        console.log(player1.signatureMove);
+                        console.log(player2.signatureMove);
+                        console.log(winner);
+                        if (winner === player1.signatureMove) {
+                            infoText.textContent = `${player1.name} WINS!`;
+                            resetGame();
+                            turn = 0;
+                        } else if (winner === player2.signatureMove) {
+                            infoText.textContent = `${player2.name} WINS!`;
+                            resetGame();
+                            turn = 0;
+                        } else if (winner === 'T') {
+                            infoText.textContent = "It's a TIE!";
+                            resetGame();
+                            turn = 0;
+                        } else {
+                            infoText.textContent = `${player1.name}'s turn, your signature move is '${player1.signatureMove}'`;
+                        }
+                        element.removeEventListener('click', element.fn, false);
+                        turn--;
+                    }
+                }, false));
+            });
+           
+        };
+
+        return { applyListeners };
+    }();
 
     const checkBoard = function() {
         // Checks for rows
@@ -64,52 +172,14 @@ const game = (function() {
             return 'T';
         } 
 
-        return null
+        return null;
     };
 
     const run = function() {
-        let winner = '';
-
-        while (true) {
-            let playerMove1 = player1.getPlayerMove(prompt(`${player1.name}, input your placement index (row, column < 3):`));
-            setMove(Number(playerMove1[0]), Number(playerMove1[1]), player1.signatureMove);
-
-            winner = checkBoard();
-            if (winner === 'O') {
-                console.log(`${player1.name} wins`);
-                break;
-            } else if (winner === 'X') {
-                console.log(`${player2.name} wins`);
-                break;
-            } else if (winner === 'T') {
-                console.log("It's a tie!")
-                break;
-            } else {
-                console.log("next part...")
-            } console.table(arrBoard);
-                
-
-            let playerMove2 = player2.getPlayerMove(prompt(`${player2.name}, input your placement index (row, column < 3):`));
-            setMove(Number(playerMove2[0]), Number(playerMove2[1]), player2.signatureMove);
-
-            winner = checkBoard();
-            if (winner === 'O') {
-                console.log(`${player1.name} wins`);
-                break;
-            } else if (winner === 'X') {
-                console.log(`${player2.name} wins`);
-                break;
-            } else if (winner === 'T') {
-                console.log("It's a tie!");
-                break;
-            } else {
-                console.log("next part...");
-            } console.table(arrBoard);
-        }
+        domBuilder.applyListeners();
     }; 
 
     return { run };
 })();
-
 
 game.run();
